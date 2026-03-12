7ec2f23fa3237597024f4e1f7e4fd4936a780e0b
Rafael Garcia <rafbgarcia@gmail.com>
2026-03-12 07:37:00 -0300

    release: v0.1.0-alpha.4

--- internal/release/release.go
+++ internal/release/release.go
@@ -6,8 +6,8 @@ import (
 )
 
 const (
-	Version          = "0.1.0-alpha.3"
-	ModuleVersion    = "v0.1.0-alpha.3"
+	Version          = "0.1.0-alpha.4"
+	ModuleVersion    = "v0.1.0-alpha.4"
 	FrameworkModule  = "github.com/rafbgarcia/rstf"
 	CLIPackage       = "@rstf/cli"
 	CreatePackage    = "create-rstf"
